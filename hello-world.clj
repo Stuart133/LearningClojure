0ddc09c0a05d913a04d6fd5f76edbb3fa0b9755f
Stuart Reid <stuart1334@gmail.com>
2021-09-08 17:00:31 +0100

    Add main defn

--- hello-world.clj
+++ hello-world.clj
@@ -1,5 +1,11 @@
+(ns hello-world.core
+  (:gen-class))
+
 (defn hello-world []
   (println "Hello, world!"))
 
 (defn say-welcome [what]
   (println "Welcome to" what))
+
+(defn -main []
+  say-welcome "Clojure"))
