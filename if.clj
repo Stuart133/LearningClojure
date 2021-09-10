156011b6fef0057bfb085dd006bcac3dc81bec2c
Stuart Reid <stuart1334@gmail.com>
2021-09-10 17:03:18 +0100

    Cond exmaple

--- if.clj
+++ if.clj
@@ -11,3 +11,8 @@
 (defn shipping-surcharge? [preferred-customer express oversized]
   (and (not preferred-customer) (or express oversized)))
 
+(defn shipping-charge [preferred-customer order-amount]
+  (cond
+    preferred-customer 0.0
+    (< order-amount 50.0) 5.0
+    (< order-amount 100.0) 10.0))
