6409b3f1a0f5f0a62f7578371e96d089762d60f3
Stuart Reid <stuart1334@gmail.com>
2021-09-21 16:52:19 +0100

    Add multimethod

--- functions.clj
+++ functions.clj
@@ -4,3 +4,20 @@
 
 (defn print-any-args [& args]
   (println "My arguments are: " args))
+
+(defn dispatch-book-format [book]
+  (cond
+    (vector? book) :vector-book
+    (contains? book :title) :standard-map
+    (contains? book :book) :alternative-map))
+
+(defmulti normalize-book dispatch-book-format)
+
+(defmethod normalize-book :vector-book [book]
+  {:title (first book) :author (second book)})
+
+(defmethod normalize-book :standard-map [book]
+  book)
+
+(defmethod normalize-book :alternative-map [book]
+  {:title (:book book) :author (:by book)})
