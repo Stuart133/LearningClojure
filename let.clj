70d6df73faf0635dfd639aaa7b708cf0a0061d61
Stuart Reid <stuart1334@gmail.com>
2021-09-23 17:03:51 +0100

    Use let to construct closures

--- let.clj
+++ let.clj
@@ -4,3 +4,20 @@
   (if (> discounted-amount min-charge)
     discounted-amount
     min-charge)))
+
+(defn compute-discount-amount [amount user-name user-discounts min-charge]
+  (let [discount-percent (user-discounts user-name)
+        discount (* amount discount-percent)
+        discounted-amount (- amount discount)]
+  (if (> discounted-amount min-charge)
+    discounted-amount
+    min-charge)))
+
+(defn mk-discount-price-f [user-name user-discounts min-charge]
+  (let [discount-percent (user-discounts user-name)]
+    (fn [amount]
+      (let [discount (* amount discount-percent)
+            discounted-amount (- amount discount)]
+        (if (> discounted-amount min-charge)
+          discounted-amount
+          min-charge)))))
